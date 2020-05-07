499660108a13d8e4dfa3ba61c02d30baa7e0acfc
unknown <nelliarchana.20@gmail.com>
2020-05-07 19:56:35 +0530

    initial commit

---
+++ flask_app/flaskblog/config.py
@@ -0,0 +1,9 @@
+import os
+class Config:
+    SECRET_KEY='b011b177715b2be46005bb24e9426269'
+    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME= 'nelliarchana.20@gmail.com'
+    MAIL_PASSWORD= 'priya@143'
